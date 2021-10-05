cf0a3c524831062e5119b3e645f554adabd40cef
ivansinabdeev <ivansinabdeev@gmail.com>
2021-10-05 04:02:39 +0200

    code for search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, { Component } from "react";
+
+import SearchBar from "./components/SearchBar/SearchBar";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    image: null,
+  };
+
+  componentDidMount() {
+    fetch(
+      "https://pixabay.com/api/?q=home&page=1&key=22555284-ed5f3516253fb9f9c4ec5f32e&image_type=photo&orientation=horizontal&per_page=12"
+    )
+      .then((res) => res.json())
+      .then((image) => this.setState({ image }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Get started!</h1>
+        {this.state.image && <p>А вот и картинка!</p>}
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
